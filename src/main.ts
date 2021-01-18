424d405138072208890e5219c16a65768b88acdd
越川 佳祐 <keisuke.koshikawa@icare.jpn.com>
2021-01-18 21:51:34 +0900

    create login function

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import { setupAuth } from '@/auth'
+import router from '@/router'
+import authConfig from '../auth_config.json'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App).use(router)
+
+function callbackRedirect (appState: any) {
+  router.push(
+    appState && appState.targetUrl ? appState.targetUrl : '/'
+  )
+}
+
+setupAuth(authConfig, callbackRedirect).then((auth) => {
+  app.use(auth)
+})
+
+app.mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div v-if="!$auth.loading.value">
+    <button v-if="!$auth.isAuthenticated.value" @click="login">Log in</button>
+    <button v-if="$auth.isAuthenticated.value" @click="logout">Log out</button>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent, inject } from 'vue'
+
+export default defineComponent({
+  name: 'About',
+  setup () {
+    const auth = inject<any>('$auth')
+
+    return {
+      login: () => {
+        auth.loginWithRedirect()
+      },
+      logout: () => {
+        auth.logout({
+          returnTo: window.location.origin
+        })
+      }
+    }
+  }
+})
+</script>
