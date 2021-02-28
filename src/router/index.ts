2bf6a3a13f910a813340ca9ca6ff677820874af1
越川 佳祐 <keisuke.koshikawa@icare.jpn.com>
2021-02-28 15:15:21 +0900

    add router-guard function

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,26 +2,31 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Login from '@/views/Login.vue'
 import Post from '@/views/Post.vue'
 import NewPost from '@/views/NewPost.vue'
+import { authorizeToken } from './guards'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/login',
     name: 'Login',
-    component: Login
+    component: Login,
+    meta: { requiresNotAuth: true }
   },
   {
     path: '/posts',
     name: 'Post',
-    component: Post
+    component: Post,
+    meta: { requiresAuth: true }
   },
   {
     path: '/posts/new',
     name: 'NewPost',
-    component: NewPost
+    component: NewPost,
+    meta: { requiresAuth: true }
   },
   {
     path: '/about',
     name: 'About',
+    meta: { requiresAuth: true },
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
@@ -30,5 +35,6 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+router.beforeEach(authorizeToken)
 
 export default router
