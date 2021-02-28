059f537b0e811b2af7552e9befdc5b2f969c964b
越川 佳祐 <keisuke.koshikawa@icare.jpn.com>
2021-02-28 15:03:35 +0900

    Update AppPost.vue

--- src/components/AppPost.vue
+++ src/components/AppPost.vue
@@ -1,5 +1,12 @@
 <template>
-  <p>{{ post }}</p>
+<div class="rounded overflow-hidden shadow-lg pt-8 mr-8">
+  <img :src="post.encoded_icatch">
+  <div class="font-bold text-xl mb-2">{{post.title}}</div>
+  <p class="text-grey-darker text-base">
+    {{ post.body }}
+  </p>
+</div>
+
 </template>
 
 <script lang="ts">
