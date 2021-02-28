93c9c2857ce72ceedd2df8d81bab97e331da5b20
越川 佳祐 <keisuke.koshikawa@icare.jpn.com>
2021-02-28 15:03:26 +0900

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
 import Post from '@/views/Post.vue'
 import NewPost from '@/views/NewPost.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
     path: '/posts',
@@ -22,9 +22,6 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
