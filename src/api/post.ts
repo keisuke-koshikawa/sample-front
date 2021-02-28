951fd18c6b89e5c756af52a3a05cd6eb1c31d00d
越川 佳祐 <keisuke.koshikawa@icare.jpn.com>
2021-02-28 22:00:37 +0900

    create Post API

--- src/api/post.ts
+++ src/api/post.ts
@@ -1,6 +1,7 @@
 import Client from '@/api/client'
 import {
-  getAuthDataFromStorage
+  getAuthDataFromStorage,
+  getAuthDataFromStorageWithFormData
 } from '@/utils/auth-data'
 
 export const getPosts = async () => {
@@ -14,7 +15,19 @@ export const getPosts = async () => {
 }
 
 export const getPost = async (id: string) => {
-  return await Client.get(`/posts/${id}`, { headers: getAuthDataFromStorage })
+  return await Client.get(`users/1/posts/${id}`, { headers: getAuthDataFromStorage })
+    .then((response) => {
+      return response.data
+    })
+}
+
+export const createPost = async (formData: any) => {
+  return await Client.post(
+    'users/1/posts', formData,
+    {
+      headers: getAuthDataFromStorageWithFormData()
+    }
+  )
     .then((response) => {
       return response.data
     })
