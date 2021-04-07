0ef712210176af3acb690ed77b9247ed7dcc9fe2
越川 佳祐 <keisuke.koshikawa@icare.jpn.com>
2021-04-07 22:40:08 +0900

    use PropType

--- src/components/AppPost.vue
+++ src/components/AppPost.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="rounded overflow-hidden shadow-lg mt-8 pt-8 mr-8">
-    <router-link :to="{ name: 'PostDetail', params: { id: post.id }}">
-      <img :src="post.encoded_icatch">
-    </router-link>
     <div class="font-bold text-xl mb-2">{{post.title}}</div>
     <p class="text-grey-darker text-base">
       {{ post.body }}
@@ -11,13 +8,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { Post } from '@/types/post'
+import { defineComponent, PropType } from 'vue'
 
 export default defineComponent({
   name: 'AppPost',
   props: {
     post: {
-      type: Object,
+      type: Object as PropType<Post>,
       required: true
     }
   }
